1b0471715959264844583acb4789967bc958151a
Sandile-Repos <sandile.developer@gmail.com>
2022-06-11 16:51:15 +0200

    custom component

--- App.js
+++ App.js
@@ -2,49 +2,17 @@ import React, { useState } from "react";
 import { Text, View, Image, TextInput } from "react-native";
 import icon from "./assets/icon.png";
 import styles from "./App.styles";
+import ImageOption from "./src/components/ImageOption";
 
 const App = () => {
   return (
     <View style={styles.root}>
       <Text style={styles.title}>Which of these is the "glass"?</Text>
       <View style={styles.optionsContainer}>
-        <View style={styles.optionContainer}>
-          <Image
-            source={{
-              uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png ",
-            }}
-            style={styles.optionImage}
-            resizeMode="contain"
-          />
-          <Text style={styles.optionText}>Glass</Text>
-        </View>
-        <View style={styles.optionContainer}>
-          <Image
-            source={{
-              uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png ",
-            }}
-            style={styles.optionImage}
-          />
-          <Text style={styles.optionText}>Glass</Text>
-        </View>
-        <View style={styles.optionContainer}>
-          <Image
-            source={{
-              uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png ",
-            }}
-            style={styles.optionImage}
-          />
-          <Text style={styles.optionText}>Glass</Text>
-        </View>
-        <View style={styles.optionContainer}>
-          <Image
-            source={{
-              uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png ",
-            }}
-            style={styles.optionImage}
-          />
-          <Text style={styles.optionText}>Glass</Text>
-        </View>
+        <ImageOption />
+        <ImageOption />
+        <ImageOption />
+        <ImageOption />
       </View>
     </View>
   );
--- App.styles.js
+++ App.styles.js
@@ -24,29 +24,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignContent: "space-between", // works with flexWrap
   },
-
-  optionContainer: {
-    //border
-    borderWidth: 2,
-    borderBottomWidth: 4,
-    borderColor: "lightgrey",
-    borderRadius: 10,
-
-    //size
-    width: "48%",
-    height: "48%",
-
-    //spacing
-    padding: 10,
-
-    alignItems: "center",
-  },
-
-  optionImage: {
-    width: "100%",
-    flex: 1,
-  },
-  optionText: {},
 });
 
 export default styles;
