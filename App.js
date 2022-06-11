f4a02c92f1c4fd2edf95573aaec32cb1c692e4f0
Sandile-Repos <sandile.developer@gmail.com>
2022-06-11 18:06:37 +0200

    render list of options

--- App.js
+++ App.js
@@ -1,31 +1,25 @@
 import React, { useState } from "react";
 import { Text, View, Image, TextInput } from "react-native";
+
 import icon from "./assets/icon.png";
 import styles from "./App.styles";
 import ImageOption from "./src/components/ImageOption";
 import question from "./assets/data/oneQuestionWithOption";
 
 const App = () => {
+  const status = "ok";
+
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>Which of these is the "glass"?</Text>
+      <Text style={styles.title}>{question.question}</Text>
       <View style={styles.optionsContainer}>
-        <ImageOption
-          image={question.options[0].image}
-          text={question.options[0].text}
-        />
-        <ImageOption
-          image={question.options[1].image}
-          text={question.options[1].text}
-        />
-        <ImageOption
-          image={question.options[2].image}
-          text={question.options[2].text}
-        />
-        <ImageOption
-          image="https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png"
-          text="cup"
-        />
+        {question.options.map((option) => (
+          <ImageOption
+            key={option.id}
+            image={option.image}
+            text={option.text}
+          />
+        ))}
       </View>
     </View>
   );
