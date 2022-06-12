2093c03e5a35b9fe164fce106b655cbca62cd42e
Sandile-Repos <sandile.developer@gmail.com>
2022-06-12 12:34:10 +0200

    quiz logic

--- App.js
+++ App.js
@@ -1,25 +1,45 @@
-import React, { useState } from "react";
-import { Text, View, Image, TextInput } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, Alert } from "react-native";
 
-import icon from "./assets/icon.png";
 import styles from "./App.styles";
 import ImageOption from "./src/components/ImageOption";
-import question from "./assets/data/oneQuestionWithOption";
 import Button from "./src/components/Button";
+// import question from "./assets/data/oneQuestionWithOption";
+import questions from "./assets/data/imageMultipleChoiceQuestions";
 
 const App = () => {
-  // const [selected, setSelected] = useState(question.options[0]);
   const [selected, setSelected] = useState(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(
+    questions[currentQuestionIndex]
+  );
+
+  useEffect(() => {
+    console.log("use effect called");
+    if (currentQuestionIndex >= questions.length) {
+      Alert.alert("You won");
+      setCurrentQuestionIndex(0);
+    } else {
+      setCurrentQuestion(questions[currentQuestionIndex]);
+    }
+  }, [currentQuestionIndex]);
 
   const onButtonPress = () => {
-    console.warn("Pressesd");
+    if (selected.correct) {
+      Alert.alert("Correct");
+      //move to next question
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setSelected(null);
+    } else {
+      Alert.alert("Wrong");
+    }
   };
 
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>{question.question}</Text>
+      <Text style={styles.title}>{currentQuestion.question}</Text>
       <View style={styles.optionsContainer}>
-        {question.options.map((option) => (
+        {currentQuestion.options.map((option) => (
           <ImageOption
             key={option.id}
             image={option.image}
